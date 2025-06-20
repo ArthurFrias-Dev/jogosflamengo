a3e8255599c6aa3905399d1099bf03302fc19bcb
ArthurFrias-Dev <ArthurFrias-Dev@users.noreply.github.com>
2025-06-20 15:02:03 -0300

    mensagem do commit

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def jogos():
         return jsonify({'erro': 'Erro ao buscar jogos'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
